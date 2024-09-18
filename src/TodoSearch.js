d4430df1558e946ae6bf1fbb17cf88a9de1b6614
Edgar Mauricio Bello Lamprea <edgarmbellol@gmail.com>
2024-09-18 16:53:28 -0500

    Segundo commit

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [valorBuscado,setValorBuscado] = React.useState('');
+
   return (
     <input
       placeholder="Buscar..."
       className="TodoSearch"
+      value={valorBuscado}
+      onChange = {(event)=> {
+        console.log("Diste click en el input");
+        console.log(event.target.value);
+        setValorBuscado(event.target.value);
+      }}
     />
   );
 }
