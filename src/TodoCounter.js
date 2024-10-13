a0b2ce8561f7c016fa67869b50e6dd5784069aa6
Edgar Mauricio Bello <edgarmbellol@gmail.com>
2024-10-13 17:36:55 -0500

    mensaje de todos compleatdosa

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,9 +1,17 @@
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
+  let texto = '';
+  if (total==completed){
+    texto = "Haz completado todas tus tareas";
+  }
+  else{
+    texto = "Has completado " + completed + " de " + total + " TODOs"; 
+  }
   return (
     <h2 className="TodoCounter">
-      Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+      {texto}
+      {/* Has completado <span>{completed}</span> de <span>{total}</span> TODOs */}
     </h2>
   );
 }
